bc18a5e21dcffc06445f0e7ef7635d2dde2fd202
Boyd van Lingen <boydvanlingen@gmail.com>
2025-09-23 15:30:21 +0200

    Blood trail wip

---
+++ Assets/BloodTrail.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class BloodTrail : MonoBehaviour
+{
+    [SerializeField] private GameObject bloodSplashPref;
+    [SerializeField] private float splashDuration;
+    [SerializeField] private int maxBloodSplashes;
+
+    private GameObject[] bloodSplashes;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
